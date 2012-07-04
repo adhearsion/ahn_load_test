3508dca29f66a6ed0fd5e94319f6404e91073b8f
Ben Langfeld <ben@langfeld.me>
2012-07-04 20:13:20 +0100

    Simplify the profiles in the target app

--- config/adhearsion.rb
+++ config/adhearsion.rb
@@ -2,34 +2,10 @@
 
 Adhearsion.config do |config|
 
-  # Centralized way to specify any Adhearsion platform or plugin configuration
-  # - Execute rake config:show to view the active configuration values
-  #
-  # To update a plugin configuration you can write either:
-  #
-  #    * Option 1
-  #        Adhearsion.config.<plugin-name> do |config|
-  #          config.<key> = <value>
-  #        end
-  #
-  #    * Option 2
-  #        Adhearsion.config do |config|
-  #          config.<plugin-name>.<key> = <value>
-  #        end
-
   config.development do |dev|
     dev.platform.logging.level = :trace
   end
 
-  ##
-  # Use with Rayo (eg Voxeo PRISM)
-  #
-  # config.punchblock.username = "" # Your XMPP JID for use with Rayo
-  # config.punchblock.password = "" # Your XMPP password
-
-  ##
-  # Use with Asterisk
-  #
   config.punchblock.platform = :asterisk # Use Asterisk
   config.punchblock.username = "adhearsion" # Your AMI username
   config.punchblock.password = "ahn123" # Your AMI password
@@ -40,30 +16,34 @@ Adhearsion.config do |config|
   #config.platform.dial_target = "Local/77@default"
 end
 
-Adhearsion::Events.draw do
-
-  # Register global handlers for events
-  #
-  # eg. Handling Punchblock events
-  # punchblock do |event|
-  #   ...
-  # end
-  #
-  # eg Handling PeerStatus AMI events
-  # ami :name => 'PeerStatus' do |event|
-  #   ...
-  # end
-  #
-end
-
 Adhearsion.router do
+  route 'SimpleProfile', :to => '1' do
+    answer
+    play "tt-monkeys"
+    execute "WaitExten", 120
+    hangup
+  end
 
-  #
-  # Specify your call routes, directing calls with particular attributes to a controller
-  #
+  route 'EarlyProfile', :to => '2' do
+    play "#{Adhearsion.config[:platform].root}/sounds/80sec-monkeys"
+    answer
+    play "tt-weasels"
+    hangup
+  end
 
-  route 'SimpleProfile', SimpleProfile, :to => '1'
-  route 'EarlyProfile', EarlyProfile, :to => '2'
-  route 'InputProfile', InputProfile, :to => '3'
-  route 'DialProfile', DialProfile, :to => '4'
+  route 'InputProfile', :to => '3' do
+    answer
+    play "#{Adhearsion.config[:platform].root}/sounds/25sec-monkeys"
+    result = ask :limit => 1, :timeout => 10000
+    play "tt-weasels"
+    hangup
+  end
+
+  route 'DialProfile', :to => '4' do
+    answer
+    dial_target = ENV['DIAL_TARGET'] || Adhearsion.config.platform.dial_target
+    logger.info "Dialing #{dial_target}"
+    dial dial_target
+    hangup
+  end
 end
